a1936bc1bf49d9118c5f2783088f5e65fdd7bff0
Aliaksei Kharko <AliakseiKharko@gmail.com>
2022-09-15 19:04:46 +0300

    feat: add done Task1 for Decision making statements

---
+++ Lessons/Lesson3/CompletedTasks/Task1.java
@@ -0,0 +1,34 @@
+public class Task1 {
+    public static void main(String[] args) {
+        int numberWeekday = 1;
+        String weekday = "";
+
+        switch (numberWeekday) {
+            case 1:
+                weekday = "Monday";
+                break;
+            case 2:
+                weekday = "Tuesday";
+                break;
+            case 3:
+                weekday = "Wednesday";
+                break;
+            case 4:
+                weekday = "Thursday";
+                break;
+            case 5:
+                weekday = "Friday";
+                break;
+            case 6:
+                weekday = "Saturday";
+                break;
+            case 7:
+                weekday = "Sunday";
+                break;
+            default:
+                weekday = "No day with this number!";
+                break;
+        }
+        System.out.println(weekday);
+    }
+}
